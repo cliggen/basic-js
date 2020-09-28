ac589832ed9ca213a2d252316aa06bf04db46c0f
Aliaksandr Matveichykau <cliggen@gmail.com>
2020-09-28 10:57:22 +0300

    feat: extended repeater solution

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,40 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    let repeatTimes = options.repeatTimes;
+    let separator = options.separator;
+    let addition = options.addition;
+    let additionRepeatTimes = options.additionRepeatTimes;
+    let additionSeparator = options.additionSeparator;
+    let result = '';
+    let addStr = '';
+    if (separator == undefined) {
+        separator = '+';
+    };
+    if (additionSeparator == undefined) {
+        additionSeparator = '|';
+    };
+    if (additionRepeatTimes == undefined && !addition) {
+        addStr = '';
+    } else if ((additionRepeatTimes == undefined && addition) || additionRepeatTimes == 1) {
+        addStr = addition;
+    } else {
+        let i = 0;
+        do {
+            addStr += addition + additionSeparator;
+            i = i + 1;
+        } while (i < additionRepeatTimes - 1);
+        addStr += addition;
+    };
+    if (repeatTimes == undefined || repeatTimes == 1) {
+        result += str + addStr;
+    } else {
+        let j = 0;
+        do {
+            result += str + addStr + separator;
+            j = j + 1;
+        } while (j < repeatTimes - 1);
+        result += str + addStr;
+    };
+    return result;
 };
-  
